42e08929aa24162f7adb3f88d40dd14c1c838e8e
iamsirid <iamsirid@gmail.com>
2022-12-19 18:30:51 +0700

    EXP01: :sparkles: add basic http server using echo

--- server.go
+++ server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
